b65b30d19ef45aaf18cc10f1068c8b609f5aa385
hawkpf <hawkpf@miamioh.edu>
2021-09-24 12:13:33 -0400

    Initial Commit

---
+++ doWork.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+backup_dir="backups"
+current_conf="config.txt"
+ezproxy_full_path=""
+ezproxy_config_name="config.txt"
+google_sheet=""
+current_google_csv="current_google.csv"
+custom_stanzas_header="custom_stanzas_header.txt"
+header="header.txt"
+old_google_csv="old_google.csv"
+production="false"
+rebuild="false"
+stanza_dir="oclc-ezproxy-database-stanzas/stanzas"
+
+#backup config file before we overwrite it
+backup_config() {
+  if [[ ! -d $backup_dir ]]; then
+    mkdir $backup_dir
+  fi
+  local timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+  cp -a $current_conf ${backup_dir}/config-${timestamp}
+  cp -a $current_google_csv $old_google_csv
+}
+
+#compare hash of files to see if they have changed
+check_hash() {
+  local original_sha=$(sha1sum $1 | awk '{print $1}')
+  local new_sha=$(sha1sum $2 | awk '{print $1}')
+  #echo $original_sha " : " $new_sha
+  if [[ "$original_sha" != "$new_sha" ]];
+  then
+    echo "yes"
+  else
+    echo "no"
+  fi
+}
+
+#pull in any changes made to the google csv and compare to old csv
+compare_csv() {
+  if [[ -f "$current_google_csv" && -f "$old_google_csv" ]]; then
+    curl -L -o $current_google_csv $google_sheet
+    if [[ $(grep 'Error 400 (Bad Request)' $current_google_csv | wc -l) -eq 0 ]]; then
+      local changed=$(check_hash $current_google_csv $old_google_csv)
+      echo "changed is: "$changed
+      if [ $changed == "yes" ]; then
+        rebuild="true"
+      fi
+    else
+      rebuild="false"
+      echo "bad request"
+    fi
+  else
+    #the files are not set up yet
+    curl -L -o $current_google_csv $google_sheet
+    cp -a $current_google_csv $old_google_csv
+  fi
+}
+
+#pull stanza repo from github
+pull_repo() {
+  if [ "$(cd $stanza_dir && git pull)" != "Already up-to-date." ];
+  then
+      rebuild="true"
+  fi
+}
+
+#rebuild the config file if changes were made to git repo or google csv
+rebuild_config() {
+cat $header > $current_conf
+cat $custom_stanzas_header >> $current_conf
+echo "
+##################################################
+#       Automated Stanzas are as follows:        #
+##################################################
+" >> $current_conf
+while IFS=', ' read -r -a array;
+do
+  if [ "${array[0]}" == "TRUE" ]
+  then
+    echo -e "\n" >> $current_conf
+    local array[1]=$(echo ${array[1]} | tr -d '[:space:]')
+    cat "${stanza_dir}/${array[1]}/stanza.txt"  >> $current_conf
+    fi
+done < $current_google_csv
+
+#trim all return lines down to a single return line in config file
+sed -i '/^$/N;/^\n$/D' $current_conf
+}
+
+replace_config_in_ezproxy_dir() {
+  cp -a ${current_conf} ${ezproxy_full_path}/${ezproxy_config_name}
+}
+
+restart_ezproxy() {
+  cd ${ezproxy_full_path}; ./ezproxy restart
+}
+
+main() {
+  pull_repo
+  compare_csv
+  if [ "$rebuild" == "true" ]; then
+    backup_config
+    rebuild_config
+    if [ "$production" == "true" ]; then
+      replace_config_in_ezproxy_dir
+      restart_ezproxy
+    fi
+  fi
+}
+
+main
+
+exit 0
---
+++ init.sh
@@ -0,0 +1,102 @@
+#!bin/bash
+stanza_dir="oclc-ezproxy-database-stanzas/stanzas"
+header="header.txt"
+extract="config_stanza_extract.txt"
+subscribed_stanza_list="subscribed_stanza.txt"
+stanzas_csv="stanzas.csv"
+stanza_list="foldernames.txt"
+original_stanzas="original_stanzas.txt"
+original_config="original_config.txt"
+custom_stanzas_header="custom_stanzas_header.txt"
+custom_stanzas="custom_stanzas.txt"
+stanzas_git_repo="https://github.com/kent-state-university-libraries/oclc-ezproxy-database-stanzas.git"
+final_config="config.txt"
+
+
+#clone stanzas git repo
+if [[ ! -d "oclc-ezproxy-database-stanzas"  ]]; then
+  git clone $stanzas_git_repo
+fi
+
+#remove multiple return lines in config file
+sed -i '/^$/N;/^\n$/D' $original_config
+
+#build a list of stanzas from the current config file.
+grep -P "^(?:U(?:RL)?\ )http(?:s)?:\/\/((?:www)?.*\.(?:com|org|edu|net)?)" $original_config | grep -ril  "$(awk '{print $2}')" $stanza_dir | cut -d'/' -f 3 | sort -uo $extract
+
+grep -P "^(?:U(?:RL)?\ )http(?:s)?:\/\/((?:www)?.*\.(?:com|org|edu|net)?)" $original_config > $original_stanzas
+#remove URL and http(s)://
+sed -i -E 's/U(RL)? http(s)?:\/\///g' $original_stanzas
+#remove first instance of a slash and anything after then sort
+sed -i 's|/.*||' $original_stanzas
+sort -uo $original_stanzas $original_stanzas
+
+
+cat /dev/null > $custom_stanzas
+while IFS= read -r -a array;
+do
+    if [ $( grep -ril "${array}" ./oclc-ezproxy-database-stanzas/stanzas/ | wc -l ) -eq 0 ];
+    then
+      echo $array >> $custom_stanzas
+    fi
+done < $original_stanzas
+
+#build custom stanza blocks from original config
+cat /dev/null > $custom_stanzas_header
+echo "
+##################################################
+#         Custom Stanzas are as follows:         #
+##################################################
+" > $custom_stanzas_header
+cat $custom_stanzas | xargs -I{} awk -vRS='\n\n' '/{}/{print $0, "\n"}' $original_config >> $custom_stanzas_header
+
+#create complete list of stanzas from repo
+ls oclc-ezproxy-database-stanzas/stanzas > $stanza_list
+
+#add urls to staza list for easier identification when using the csv to control subscriptions
+cat /dev/null > $stanzas_csv
+while IFS= read -r -a array;
+do
+  a=$(grep -P "^(?:U(?:RL)?\ )http(?:s)?:\/\/((?:www)?.*\.(?:com|org|edu|net)?)" oclc-ezproxy-database-stanzas/stanzas/$array/stanza.txt  | sed -E 's/U(RL)? http(s)?:\/\///g' | sed 's|/.*||')
+
+  echo -e "FALSE,"$array","$a >> $stanzas_csv
+done < $stanza_list
+
+cat /dev/null > $subscribed_stanza_list
+while IFS= read -r -a array;
+do
+  echo -e "TRUE,"$array >> $subscribed_stanza_list
+done < $extract
+
+#replace false with true
+while IFS= read -r -a array;
+do
+  stanza_name=$(echo $array | sed 's/TRUE,//')
+  sed -i "s/FALSE,${stanza_name}/TRUE,${stanza_name}/g" $stanzas_csv
+done < $subscribed_stanza_list
+
+#build config file
+cat $header > $final_config
+cat $custom_stanzas_header >> $final_config
+echo "
+##################################################
+#       Automated Stanzas are as follows:        #
+##################################################
+" >> $final_config
+while IFS=', ' read -r -a array;
+do
+  if [ "${array[0]}" == "TRUE" ]
+  then
+    echo -e "\n" >> $final_config
+    array[1]=$(echo ${array[1]} | tr -d '[:space:]')
+    cat "${stanza_dir}/${array[1]}/stanza.txt"  >> $final_config
+    fi
+done < $stanzas_csv
+
+#remove multiple return lines in config file
+sed -i '/^$/N;/^\n$/D' $final_config
+
+#add url to csv for easier identification
+
+
+exit 0
